e8366bca58e5f96328ba4956f8b9c724eab32851
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-08 17:28:47 +0100

    Eugh more css

--- about.php
+++ about.php
@@ -7,7 +7,6 @@
   <link rel="shortcut icon" href="img/favicon.ico">
   <link rel="stylesheet" href="css/custom.css">
   <script src="https://use.fontawesome.com/8b059409f7.js"></script>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
   <script src="js/jquery-3.6.0.js"></script>
 </head>
 <body>
