5c8d325b6aef4397eb83cd28d3314f291ad310aa
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-08 17:18:56 +0100

    Fonts and css again!

--- include/header.php
+++ include/header.php
@@ -1,6 +1,9 @@
   <!DOCTYPE html>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
   <div class="header">
     <h1>Leanne Jay</h1>
-    <h1 id="undertext">Photography | Technology </h1>
+    <h1 id="undertext">Photography&nbsp;&nbsp;|&nbsp;&nbsp;Technology </h1>
   </div>
   </html>
