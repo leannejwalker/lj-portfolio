d54d60b55c5aff46d1aba31ab2fb0bd232cdcff8
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-08 17:10:05 +0100

    Changing around the css

--- include/header.php
+++ include/header.php
@@ -1,13 +1,6 @@
   <!DOCTYPE html>
-  <script>
-
-        window.onbeforeunload = function () {
-            window.scrollTo(0, 0);
-        }; 
-
-  </script>
   <div class="header">
     <h1>Leanne Jay</h1>
-    <h1 id="undertext">Photography        |         Technology </h1>
+    <h1 id="undertext">Photography | Technology </h1>
   </div>
   </html>
