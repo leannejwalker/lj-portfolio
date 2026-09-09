f89ad3a24c28e8577540f88096b0025a3e5c10fa
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-09 14:02:58 +0100

    Updates to header

--- include/header.php
+++ include/header.php
@@ -1,6 +1,6 @@
   <!DOCTYPE html>
   <div class="header">
-    <h1>Leanne Jay</h1>
+    <img class="logo" src="img/lj.jpg" alt="logo">
     <h1 id="undertext">Photography&nbsp;&nbsp;|&nbsp;&nbsp;Technology </h1>
   </div>
   </html>
