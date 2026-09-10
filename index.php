7316e8ffb62debd4483f13b032b414dcd7825ca0
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-10 17:29:28 +0100

    .

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <script src="https://use.fontawesome.com/8b059409f7.js"></script>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
   <?php include 'include/sidenav.php'; ?>
