9fcd7d434802b41c2778b26fdf1f19d102ca490e
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-10 14:00:50 +0100

    .

--- include/sidenav.php
+++ include/sidenav.php
@@ -1,6 +1,6 @@
   <!DOCTYPE html>
+  <img class="logo" src="img/lj.png" alt="logo">
   <div class="sidenav">
-    <img class="logo" src="img/lj.png" alt="logo">
     <a href="#">Photography</a>
     <a href="#">Projects</a>
     <a href="#"></a>
