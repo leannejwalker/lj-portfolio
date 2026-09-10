90fc4558b766b0690c138f67298182ae2213582f
leannejwalker <89186472+leannejwalker@users.noreply.github.com>
2026-09-10 12:30:41 +0100

    .

--- include/header.php
+++ include/header.php
@@ -1,6 +1,5 @@
   <!DOCTYPE html>
   <div class="header">
-    <img class="logo" src="img/lj.jpg" alt="logo">
-    <h1 id="undertext">Photography&nbsp;&nbsp;|&nbsp;&nbsp;Technology </h1>
+    <img class="logo" src="img/lj.png" alt="logo">
   </div>
   </html>
